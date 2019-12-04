f5cba241e22f0918f967249f8f254b6d6722ccce
Imran Siddique <isiddi5@uic.edu>
2019-12-04 19:38:47 +0000

    added routes 4

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ express()
     .get('/customers', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM Customer');
+      const result = await client.query('SELECT * FROM test_table');
       const results =  (result) ? result.rows : [];
       res.json(results);
       client.release();
