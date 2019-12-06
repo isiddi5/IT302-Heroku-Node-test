607bbf63743903545c7a4138d330012c22d1a2a8
Imran Siddique <isiddi5@uic.edu>
2019-12-06 18:20:29 +0000

    added employee data

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: "postgres://gzbuqqyafwsbgp:5a238821cf1bdb550f0c138371a68166c12acbbc62343f4d13ed62b3bc993223@ec2-174-129-229-106.compute-1.amazonaws.com:5432/dantp8tri79sji",
   ssl: true
-});
+}); 
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -15,6 +16,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', async (req, res) => {
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -27,7 +29,6 @@ express()
       res.send("Error " + err);
     }
   })
-  .get('/times', (req, res) => res.send(showTimes()))
 
     .get('/customers', async (req, res) => {
     try {
@@ -41,17 +42,22 @@ express()
       res.send("Error " + err);
     }
   })
+    .get('/employees', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM public."Employee"');
+      const results =  (result) ? result.rows : [];
+      res.json(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
-}
+
 
     
 
